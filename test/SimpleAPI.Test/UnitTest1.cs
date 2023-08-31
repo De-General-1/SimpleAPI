99863ba2056d9ca23d7ea37e3b1330a52703a00f
De-General <aboagyefafa12@gmail.com>
2023-08-31 17:53:08 +0100

    initial commit

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -1,7 +1,16 @@
 namespace SimpleAPI.Test;
+using SimpleAPI.Controllers;
 
 public class UnitTest1
 {
+    ValuesController controller = new ValuesController();
+
+    [Fact]
+    public void GetReturnsMyName()
+    {
+        var returnValue = controller.Get(1);
+        Assert.Equal("De-General", returnValue.Value);
+    }
     [Fact]
     public void Test1()
     {
